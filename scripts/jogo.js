cf9403d64454c037ebc09b450a53c3d22292d732
Gabriel Succi <zicanony@gmail.com>
2021-08-09 15:01:00 -0300

    Desenhando o Bird.

--- scripts/jogo.js
+++ scripts/jogo.js
@@ -1,6 +1,6 @@
 console.log("FLAPPY BIRD");
 
-const canvas = document.getElementsById("canvas");
+const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 const sprites = new Image();
@@ -8,14 +8,15 @@ sprites.src = "./assets/img/sprites.png";
 
 
 
-    function loop() {
-        ctx.drawImage(
-            sprites,
-            0, 0,
-            34, 24,
-            0, 0,
-            34, 24,
-            );
-    }
+function loop() {
+    ctx.drawImage(
+        sprites,
+        0, 0, // SpriteY e SpriteY
+        33, 24, // Tamanho do recorte na Sprite
+        10, 50, // Local de desenho no Canvas
+        34, 24, // Tamanho do Sprite
+        );
+        requestAnimationFrame(loop);
+}
 
-    requestAnimationFrame(loop);
+loop();
